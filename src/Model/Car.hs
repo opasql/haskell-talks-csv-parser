f81e1423dcedea89ae9136894b512f36950d0b6d
Oleg Pascal <61763119+opasql@users.noreply.github.com>
2024-04-04 16:32:50 +0300

    Add newtypes and comments

--- src/Model/Car.hs
+++ src/Model/Car.hs
@@ -6,15 +6,33 @@ import           Util
 
 data Car = Car
   { carManufacturer :: Text
+    -- ^ Can't be an empty string
   , carModel        :: Text
+    -- ^ Can't be an empty string
   , carOwnerName    :: Maybe Text
-  , carYear         :: Int
-  , carMilage       :: Double
+    -- ^ This one can be omitted
+  , carYear         :: Year
+    -- ^ A proper range for a year would be 1900 - 2025
+  , carMilage       :: Milage
+    -- ^ Milage can't be negative
   , carColor        :: Color
-  , carHadAccidents :: Bool
+  , carHadAccidents :: HadAccidents
   , carUsedOrNew    :: UsedOrNew
-  , carPrice        :: Double
+  , carPrice        :: Price
+    -- ^ Price can't be negative
   } deriving Show
+
+newtype Year = Year { unYear :: Int }
+  deriving Show
+
+newtype Milage = Milage { unMilage :: Double }
+  deriving Show
+
+newtype Price = Price { unPrice :: Double }
+  deriving Show
+
+newtype HadAccidents = HadAccidents { unHadAccidents :: Double }
+  deriving Show
 
 data Color =
     White
